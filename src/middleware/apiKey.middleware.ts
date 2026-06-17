91c86a7591279ccdfd4cf81e9bdafd9560ba24dc
Bassam-Elsaied <93853900+Bassam-Elsaied@users.noreply.github.com>
2026-06-18 01:19:40 +0300

    skip api key

--- src/middleware/apiKey.middleware.ts
+++ src/middleware/apiKey.middleware.ts
@@ -3,7 +3,7 @@ import { decryptDataApi } from "../utils/decrypt";
 import { logger } from "../utils/logger";
 import { pickLocalized, sendApiError } from "../utils/apiErrorResponse";
 import { ApiErrors } from "../i18n/apiErrors";
-import { isPaymentTestRoutesEnabled } from "../utils/devFlags";
+import { isPaymentTestRoutesEnabled, isApiKeyValidationSkipped } from "../utils/devFlags";
 
 require("dotenv").config();
 
@@ -70,7 +70,7 @@ function isFullyOpenPath(req: Request): boolean {
 }
 
 export function decryptApiKey(req: Request, res: Response, next: NextFunction) {
-  if (process.env.NODE_ENV === "development") {
+  if (isApiKeyValidationSkipped()) {
     return next();
   }
 
